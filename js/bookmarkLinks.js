4da003197ac2002b2fe34a3d7166c3b204056310
codencoding <36287664+Loegrath@users.noreply.github.com>
2022-10-16 18:29:08 -0500

    Link saving and removal functionalities now working!

---
+++ js/bookmarkLinks.js
@@ -0,0 +1,81 @@
+let inputTempLinkValueElem = document.getElementById('inputTempLinkValue');
+let inputTempLinkHrefElem = document.getElementById('inputTempLinkHref');
+let inputTempLinkColElem = document.getElementById('inputTempLinkCol');
+let btnTempLinkSubmitElem = document.getElementById('btnTempLinkSubmit');
+let linksBookmarkTabContainerElem = document.getElementById('linksBookmarkTabContainer');
+
+let col0TempLinks;
+let col1TempLinks;
+let col2TempLinks;
+
+loadTempLinkElems();
+
+btnTempLinkSubmitElem.addEventListener("click", () => {
+    addTempLinkElem();
+})
+
+function generateTempLinkElem(label, href) {
+    let elem = document.createElement("div");
+    elem.className = "btn-group w-100 pb-2";
+    elem.innerHTML = "<div class=\"btn-group w-100 pb-2\"><a href=\"" + href + "\" class=\"btn btn-danger w-75\">" + label + "</a><button type=\"button\" class=\"btn btn-danger dropdown-toggle dropdown-toggle-split\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\"><span class=\"visually-hidden\">Toggle Dropdown</span></button><ul class=\"dropdown-menu\"><li><a class=\"dropdown-item\" href=\"#\">Move link up</a></li><li><a class=\"dropdown-item\" href=\"#\">Move link down</a></li><li><hr class=\"dropdown-divider\"></li><li><a onclick=\"removeSelfTempLink(this);\" class=\"dropdown-item text-danger\" href=\"#\">Remove link</a></li></ul></div>";
+
+    return elem.firstChild;
+}
+
+function addTempLinkElem() {
+    let linkLabel = inputTempLinkValueElem.value;
+    let linkHref = inputTempLinkHrefElem.value;
+    let linkCol = inputTempLinkColElem.value;
+    console.log("adding temp link elem");
+    console.log("linkLabel: " + linkLabel);
+    console.log("linkHref: " + linkHref);
+    console.log("linkCol: " + linkCol);
+
+    let createdElem = generateTempLinkElem(linkLabel, linkHref);
+    console.log("created elem: ", createdElem);
+    
+    switch (linkCol) {
+        case "0":
+            col0TempLinks.appendChild(createdElem);
+            break;
+        case "1":
+            col1TempLinks.appendChild(createdElem);
+            break;
+        case "2":
+            col2TempLinks.appendChild(createdElem);
+            break;
+    
+        default:
+            break;
+    }
+
+    saveTempLinkElems();
+}
+
+function removeSelfTempLink(e) {
+    console.log("Removing...");
+    e.parentNode.parentNode.parentNode.remove();
+
+    saveTempLinkElems();
+}
+
+function extractTempLinkElems() {
+    return linksBookmarkTabContainerElem.innerHTML;
+}
+
+function saveTempLinkElems() {
+    let strTempLinkElems = extractTempLinkElems();
+
+    localStorage.setItem("temp-link-elems", strTempLinkElems);
+}
+
+function loadTempLinkElems() {
+    let strTempLinkElems = localStorage.getItem("temp-link-elems");
+    linksBookmarkTabContainerElem.innerHTML = strTempLinkElems;
+
+    col0TempLinks = document.getElementById("col0TempLinks");
+    col1TempLinks = document.getElementById("col1TempLinks");
+    col2TempLinks = document.getElementById("col2TempLinks");
+}
+
+console.log("bookmarksLinks.js loaded");
