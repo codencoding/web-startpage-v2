4359dcbf31289c1a5fe7bf1daf657c5ab9b8532e
codencoding <36287664+Loegrath@users.noreply.github.com>
2022-10-16 19:03:53 -0500

    Moving links up and down functionality working

--- js/bookmarkLinks.js
+++ js/bookmarkLinks.js
@@ -17,7 +17,7 @@ btnTempLinkSubmitElem.addEventListener("click", () => {
 function generateTempLinkElem(label, href) {
     let elem = document.createElement("div");
     elem.className = "btn-group w-100 pb-2";
-    elem.innerHTML = "<div class=\"btn-group w-100 pb-2\"><a href=\"" + href + "\" class=\"btn btn-danger w-75\">" + label + "</a><button type=\"button\" class=\"btn btn-danger dropdown-toggle dropdown-toggle-split\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\"><span class=\"visually-hidden\">Toggle Dropdown</span></button><ul class=\"dropdown-menu\"><li><a class=\"dropdown-item\" href=\"#\">Move link up</a></li><li><a class=\"dropdown-item\" href=\"#\">Move link down</a></li><li><hr class=\"dropdown-divider\"></li><li><a onclick=\"removeSelfTempLink(this);\" class=\"dropdown-item text-danger\" href=\"#\">Remove link</a></li></ul></div>";
+    elem.innerHTML = "<div class=\"btn-group w-100 pb-2\"><a href=\"" + href + "\" class=\"btn btn-danger w-75\">" + label + "</a><button type=\"button\" class=\"btn btn-danger dropdown-toggle dropdown-toggle-split\" data-bs-toggle=\"dropdown\" aria-expanded=\"false\"><span class=\"visually-hidden\">Toggle Dropdown</span></button><ul class=\"dropdown-menu\"><li><a onclick=\"moveLink(this, \'up\');\" class=\"dropdown-item\" href=\"#\">Move link up</a></li><li><a onclick=\"moveLink(this, \'down\');\" class=\"dropdown-item\" href=\"#\">Move link down</a></li><li><hr class=\"dropdown-divider\"></li><li><a onclick=\"removeSelfTempLink(this);\" class=\"dropdown-item text-danger\" href=\"#\">Remove link</a></li></ul></div>";
 
     return elem.firstChild;
 }
@@ -77,5 +77,32 @@ function loadTempLinkElems() {
     col1TempLinks = document.getElementById("col1TempLinks");
     col2TempLinks = document.getElementById("col2TempLinks");
 }
+
+function moveLink(elemLink, strDirection) {
+    let elemBtn = elemLink.parentElement.parentElement.parentElement;
+
+    let elemPrevSibling = elemBtn.previousElementSibling;
+    let elemNextSibling = elemBtn.nextElementSibling;
+    let elemParent = elemBtn.parentNode;
+
+    switch (strDirection) {
+        case "up":
+            if (elemPrevSibling == null) {
+                break;
+            }
+            elemParent.insertBefore(elemBtn, elemPrevSibling);
+            break;
+        case "down":
+            if (elemNextSibling == null) {
+                break;
+            }
+            elemParent.insertBefore(elemNextSibling, elemBtn);
+            break;
+    
+        default:
+            break;
+    }
+}
+
 
 console.log("bookmarksLinks.js loaded");
